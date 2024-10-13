7aa69022de1cbe7f880512e20b3741a26515a356
malikkurtz <malikkurtz129@gmail.com>
2024-10-13 16:17:51 -0400

    something was modified

---
+++ Math/Calculus/Calculus.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+class Calculus {
+
+    static double limit(function<double(double)>, double valueToApproach) {
+
+    }
+};
+
+
+double f(double x) {
+    return x*x + 1;
+}
