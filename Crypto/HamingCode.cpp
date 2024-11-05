63ed06245b35c473812e255bed79056e83b5d73c
malikkurtz <malikkurtz129@gmail.com>
2024-11-05 13:37:56 -0500

    finished hamming code implementation

--- Crypto/HamingCode.cpp
+++ Crypto/HamingCode.cpp
@@ -46,29 +46,151 @@ string vecBoolToString(vector<bool> vector_code) {
 }
 
 
-// number of redundancy bits = log2()
+
+vector<bool> encode(vector<bool> input) {
+
+    int r = 0;
+    int m = input.size();
 
 
-int calculateNumParityBits(vector<bool> input) {
-    int numParityBits = ceil(log(input.size())) + 1;
+    // Calculate number of parity bits
+    // 1 << r is equal to 2^r, so while 2^r is less than m + r + 1, increment r
+    while (!((1 << r) >= (m + r + 1))) {
+        r++;
+    }
 
-    return numParityBits;
+    //initialize a vector that will hold our data bits plus our redundant bits
+    vector<bool> toReturn(m + r);
+
+    
+    int dataIndex = 0;
+    // For every position in our output vector
+    for (int i = 1; i <= toReturn.size(); i++) {
+        // check if this is a position for a reserved bit
+        if ((i & (i-1)) == 0) {
+            // if it is, do nothing
+            continue;
+        }
+        // if its not reserved for a parity bit
+        else {
+            toReturn[i-1] = input[dataIndex];
+            dataIndex++;
+        }
+    }
+
+    int parity = 0;
+    // for every position in our output vector
+    for (int i = 1; i <= toReturn.size(); i <<= 1) { 
+        parity = 0;
+        for (int j = 1; j <= toReturn.size(); j++) {
+            if (j & i) { 
+                parity ^= toReturn[j - 1];
+            }
+        }
+        toReturn[i - 1] = parity; 
+    }
+    
+
+    return toReturn;
 }
 
 
-int main() {
-    string message = "a";
-    vector<bool> encoded = stringToVecBool(message);
+vector<bool> flipBit(vector<bool> encoded) {
+    int min = 0;
+    int max = encoded.size() - 1;
+    int random_num = min + rand() % (max - min + 1);
 
-    for (bool b : encoded) {
+    encoded[random_num] = encoded[random_num] ^ 1;
+
+    return encoded;
+}
+
+
+vector<bool> decode(vector<bool> noisy_encoded) {
+    vector<bool> correctEncoding = noisy_encoded;
+
+    vector<bool> decoded = {};
+
+    int errorSyndrome = 0;
+
+    for (int i = 1; i <= noisy_encoded.size(); i <<= 1) { 
+
+        int actualParity = 0;
+        for (int j = 1; j <= noisy_encoded.size(); j++) {
+            if (j & i) { 
+                actualParity ^= noisy_encoded[j - 1];
+            }
+        }
+
+        if (actualParity != 0) {
+            errorSyndrome += i;
+        }
+    }
+
+    if (errorSyndrome != 0) {
+        correctEncoding[errorSyndrome-1] = correctEncoding[errorSyndrome-1] ^ 1;
+    }
+
+    // For every position in our output vector
+    for (int i = 1; i <= correctEncoding.size(); i++) {
+        // check if this is a position for a reserved bit
+        if ((i & (i-1)) == 0) {
+            // if it is, do nothing
+            continue;
+        }
+        // if its not reserved for a parity bit
+        else {
+            decoded.push_back(correctEncoding[i-1]);
+        }
+    }
+
+    return decoded;
+    
+}
+
+
+
+
+int main() {
+
+    srand(time(0)); // Seed with the current time
+
+    string message;
+    getline(cin, message);
+    vector<bool> asVecBool = stringToVecBool(message);
+
+    cout << "Original Message: " << message << endl;
+
+    cout << "Original Message in Binary: ";
+    for (bool b : asVecBool) {
         cout << b;
     } cout << endl;
 
-    string decoded = vecBoolToString(encoded);
+    vector<bool> encoded = encode(asVecBool);
 
-    cout << decoded << endl;
-    cout << calculateNumParityBits(encoded);
 
+
+    cout << "Encoded Message in Binary:  ";
+    for (bool b : encoded) {
+        cout << b;
+    } cout << endl;
+    
+
+    vector<bool> noisy_encoded = flipBit(encoded);
+
+    cout << "Noisy Message in Binary:    ";
+    for (bool b : noisy_encoded) {
+        cout << b;
+    } cout << endl;
+
+    vector<bool> decoded = decode(noisy_encoded);
+
+    cout << "Decoded Message in Binary:  ";
+    for (bool b : decoded) {
+        cout << b;
+    } cout << endl;
+
+    cout << "Decoded Message: " << vecBoolToString(decoded);
     
 
     return 0;
